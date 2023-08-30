a21dba48cce4d5282edf433847dd09791ca5a1c9
aflr <143635386+aflr@users.noreply.github.com>
2023-08-30 20:21:39 +0200

    Update Dont-panic-episode-1.java

--- Medium/Dont-panic-episode-1/Dont-panic-episode-1.java
+++ Medium/Dont-panic-episode-1/Dont-panic-episode-1.java
@@ -2,10 +2,6 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player {
 
     public static void main(String args[]) {
