0455bb8d465dc35e74984de557e3dc233055754c
aflr <143635386+aflr@users.noreply.github.com>
2023-08-30 19:05:52 +0200

    Create Mars-lander-episode-1.cpp

---
+++ Fácil/Mars-lander-episode-1/Mars-lander-episode-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int surface_n; // the number of points used to draw the surface of Mars.
+    cin >> surface_n; cin.ignore();
+    for (int i = 0; i < surface_n; i++) {
+        int land_x; // X coordinate of a surface point. (0 to 6999)
+        int land_y; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        cin >> land_x >> land_y; cin.ignore();
+    }
+    // game loop
+    while (1) {
+        int x;
+        int y;
+        int h_speed; // the horizontal speed (in m/s), can be negative.
+        int v_speed; // the vertical speed (in m/s), can be negative.
+        int fuel; // the quantity of remaining fuel in liters.
+        int rotate; // the rotation angle in degrees (-90 to 90).
+        int power; // the thrust power (0 to 4).
+        cin >> x >> y >> h_speed >> v_speed >> fuel >> rotate >> power; cin.ignore();
+
+        int p = 2;
+        if (v_speed < -35)
+            p = 4;
+        else if (v_speed < -10)
+            p = 3;
+
+        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+        cout << "0 " << p << endl;
+    }
+}
